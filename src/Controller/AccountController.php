f2a609f5ada85916eb330622f9be2800de0c84ff
Bmalik73 <bmalik01@live.fr>
2023-02-08 14:11:26 +0100

    update style

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
-    #[Route('/account', name: 'app_account')]
+    #[Route('/compte', name: 'app_account')]
     public function index(): Response
     {
         return $this->render('account/index.html.twig', [
